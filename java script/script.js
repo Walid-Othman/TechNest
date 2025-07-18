c19d394fdae985f46dd0f542ee401bee6dacc8f8
Walid-Othman <waleedosman20111996@gmail.com>
2025-07-18 18:43:32 +0300

    work in html css and javascript

---
+++ java script/script.js
@@ -0,0 +1,19 @@
+
+
+const typed = new Typed('#typed', {
+  strings: ['Discover top brands,latest models, and unbeatab deals all in one place.'],
+  typeSpeed: 60,
+  backSpeed:30,
+  backDelay:2000,
+  startDelay:500,
+  loop:Infinity
+});
+const typed_tow = new Typed('#discrabtion-one', {
+  strings: ['smart gadgets, accessories, and tech essentials.'],
+  typeSpeed: 60,
+  backSpeed:30,
+  backDelay:2000,
+  startDelay:500,
+  loop:Infinity
+});
+
